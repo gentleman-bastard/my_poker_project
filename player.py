f8ef085a5d3bf26754e62d814f6c452096ecac6b
Ross Roberts <ross123roberts@gmail.com>
2020-05-22 13:44:44 -0400

    player class for storing hole cards and chips

--- player.py
+++ player.py
@@ -1,16 +1,12 @@
 from cards import Hand
 
-
 class Player(object):
-    def __init__(self, name, chips = 1000, hole = Hand()):
+    def __init__(self, name, chips = 1000):
         self.name = name
         self.chips = chips
-        self.hole = hole
+        self.hole = Hand()
 
     def __repr__(self):
         return f'{self.name}: {self.chips} chips. Cards in the hole: \n{self.hole}'
 
-    def bet(self, amount, pot):
-        self.chips - amount
-        pot + amount
 
