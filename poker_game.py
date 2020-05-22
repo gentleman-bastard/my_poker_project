a430c67c7f4c403bcc4fd003b62b2ec017f12e01
Ross Roberts <ross123roberts@gmail.com>
2020-05-22 13:46:36 -0400

    created game class for storing blinds, players and poker action methods

--- poker_game.py
+++ poker_game.py
@@ -1,9 +1,41 @@
-from cards import Card, Deck, Hand
-from player import Player
+from cards import Deck
+from player import Player as Player
 import logging
+from typing import List, Set, Dict, Tuple
+import datetime
 from itertools import combinations
 
+class Game(object):
+    def __init__(self, logger: logging.getLogger(),player_names: List[str], small_blind: int, big_blind: int):
+        self.logger = logger
+        self.flop = []
+        self.turn = []
+        self.river = []
+        self.dead = []
+        self.deck = Deck()
+        self.player_names = player_names
 
+    @property
+    def players(self):
+        return [Player(player_name) for player_name in self.player_names]
+
+    @property
+    def table_cards(self):
+        flop_plus_turn = self.flop.extend(self.turn)
+        return flop_plus_turn.extend(self.river)
+
+    def deal_preflop(self, players):
+        """deals 2 pre flop (hole) cards to each player"""
+        for i in range(2):
+            for player in players:
+                self.deck.move_cards(player.hole, 1)
+
+
+
+
+
+logger = logging.getLogger('pokergame')
+logger.setLevel(logging.DEBUG)
 
 deck = Deck()
 
@@ -13,47 +45,30 @@ deck.shuffle()
 
 names = ['Ross', 'Stew', 'Alex', 'Johnny']
 
+
+
 players = [Player(name) for name in names]
 
 print("the following people will be playing:")
 for player in players:
     print(player.name)
 
-
+#TODO: deal one card at a time and circle twice
+logger.debug('dealing hole cards to each player')
 for player in players:
     deck.move_cards(player.hole, 2)
 
-for player in players:
-    print(f'{player.name} has:\n {player.hole}')
 
-
-
-
-pot = 0
 
 for player in players:
-    amount = int(input(f"{player}:\nHow much would you like to bet?\n"))
-    player.bet(amount, pot)
-
-print(f'There is now ${pot} in the pot')
+    print(f'{player}')
 
 
-"""
-dead = Hand()
-table = Hand()
+logger.debug('dealing flop')
 
-deck.move_cards(dead, 1)
-deck.move_cards(table, 3)
-print(f'the flop: \n{table}')
 
-deck.move_cards(dead, 1)
-deck.move_cards(table, 1)
-print(f'after the turn: /n{table}')
 
-deck.move_cards(dead, 1)
-deck.move_cards(table, 1)
-print(f'after the river: /n{table}')
-"""
+
 
 
 
