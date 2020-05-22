395b2acb3d75c062505864afda9d1273d3a7d3fb
Ross Roberts <ross123roberts@gmail.com>
2020-05-22 15:13:12 -0400

    nothing really

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from game import Game
 from logging import getLogger
 
-def main():
-    n_players = input("How many people will be playing?")
-    player_names = [input("What is your name?") for n in n_players]
+class GameRunner:
+    def __init__(self, logger = getLogger('PokerGame')):
+        self.logger = logger
 
-    game = Game(getLogger('game_log'), player_names, 10, 20)
+    def run(self):
+        n_players = input("How many people will be playing?")
+        player_names = [input("What is your name?") for n in n_players]
+
+        self.logger.debug('instantiating game instance')
+        game = Game(getLogger('game_log'), player_names, 10, 20)
+
+
+
+
+if __name__ == '__main__':
+    GameRunner.run()
+
+
