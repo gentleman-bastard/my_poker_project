0400a86517e3b06273e3ccc3998fef304448de74
Ross Roberts <ross123roberts@gmail.com>
2020-05-24 16:33:49 -0400

    assert statements for game functions

--- test/test_game.py
+++ test/test_game.py
@@ -6,5 +6,7 @@ def test_game():
     game_test = Game(['Ross', 'Stew', 'Alex', 'Johnny'], 25, 50)
 
     assert len(game_test.players) == 4
-    assertg
+    assert game_test.players[0].name == 'Ross'
+
+
 
