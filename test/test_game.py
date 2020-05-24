557ce6d376b17042e4041988320e435d486519cb
Ross Roberts <ross123roberts@gmail.com>
2020-05-24 15:58:31 -0400

    test file for unit testing game methods

---
+++ test/test_game.py
@@ -0,0 +1,10 @@
+from game import Game
+import pytest
+
+
+def test_game():
+    game_test = Game(['Ross', 'Stew', 'Alex', 'Johnny'], 25, 50)
+
+    assert len(game_test.players) == 4
+    assertg
+
