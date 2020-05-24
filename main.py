aeefbde1419d1eb559b0d283c335aa20ab5ecf71
Ross Roberts <ross123roberts@gmail.com>
2020-05-24 11:17:35 -0400

    newline

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from game import Game
 
 
 def run():
-    n_players = int(input("How many people will be player?\n"))
+    n_players = int(input("How many people will be playing?\n"))
     player_names = [input("What is your name?\n") for i in range(n_players)]
     game = Game(player_names, 10, 20)
     game.run()
