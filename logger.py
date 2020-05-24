d05270517ff419910b6e3c7d88b8883994a8364a
Ross Roberts <ross123roberts@gmail.com>
2020-05-24 09:46:31 -0400

    configured logger root level

--- logger.py
+++ logger.py
@@ -1,20 +1,16 @@
 import logging
 
 class Logger:
-    def __init__(self, name, level = "DEBUG"):
+    def __init__(self, name):
         self.name = name
-        self.level = level
         self.logger = None
 
-    def set_level(self):
-        if self.level == '' or self.level is None:
-            return logging.INFO
-        else:
-            return self.level
-
     def get_logger(self):
+        logging.basicConfig()
+        logging.root.setLevel(logging.NOTSET)
         self.logger = logging.getLogger(self.name)
-        self.logger.setLevel(self.set_level())
+
+        self.logger.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%asctime)s - %(name)s - %(levelname)s - %(message)s')
         console_logging = logging.StreamHandler()
         console_logging.setFormatter(formatter)
